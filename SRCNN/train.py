f4fa2af624285b3fb1d956f874326b6b403aaa6f
vinzentbeer <vinzent.beer@gmail.com>
2025-02-08 17:17:57 +0100

    add trained model, directml

--- SRCNN/train.py
+++ SRCNN/train.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     device = torch_directml.device()
     torch.set_default_device(device)
-    torch.manual_seed(args.seed)
+    #torch.manual_seed(args.seed)
 
     model = SRCNN().to(device)
     criterion = nn.MSELoss()
